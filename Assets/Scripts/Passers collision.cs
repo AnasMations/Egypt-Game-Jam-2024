3df5bcfed3fb9ecbb2ec8ead5ae1e20d4658aaa9
DESKTOP-FKI4481\zeyad <zeyad.medhat2000@gmail.com>
2024-02-20 20:08:11 +0200

    - Added Basic movement and scene

---
+++ Assets/Scripts/Passers collision.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    [SerializeField] ProgressBar PeaceMeter;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PeaceMeter.current = PeaceMeter.current - 5;
+    }
+
+    //private void OnTriggerEnter2D(Collider2D collision)
+    //{
+    //    string collidertag = collision.gameObject.tag;
+    //    print("hello");
+    //    if (collidertag == "Passers")
+    //    {
+    //        print("hello");
+    //        PeaceMeter.current = PeaceMeter.current - 5;
+    //    }
+    //}
+    void OnCollisionEnter(Collision collision)
+    {
+        string collidertag = collision.gameObject.tag;
+        print("hello");
+        if (collidertag == "Passers")
+        {
+            print("hello");
+            PeaceMeter.current = PeaceMeter.current - 5;
+        }
+    }
+}
