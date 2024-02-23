d041b7f27b4fac3d60aa47e24ad598b3bde35bba
DESKTOP-FKI4481\zeyad <zeyad.medhat2000@gmail.com>
2024-02-23 13:23:15 +0200

    - Made an audio play randomly once the player is close enough

--- Assets/Scripts/Activate Audio.cs
+++ Assets/Scripts/Activate Audio.cs
@@ -7,16 +7,36 @@ public class ActivateAudio : MonoBehaviour
     // Gather the audioclips in a list\
     // make them play randomly when you come near a passer?
     bool playaudio;
-    public List<AudioClip> audioSources;
+    public List<AudioClip> audioclips;
+    private AudioClip audioclip;
+    private GameObject leobject;
+    public GameObject player;
+    bool isplaying;
     // Start is called before the first frame update
     void Start()
     {
-        
+       int audio = Random.Range(0, audioclips.Count);
+       audioclip = audioclips[audio];
+       leobject = gameObject;
+        isplaying = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 myloc;
+        Quaternion myrot;
+        Vector3 hisloc;
+        Quaternion hisrot;
+        leobject.transform.GetPositionAndRotation(out myloc, out myrot);
+        player.transform.GetPositionAndRotation(out hisloc, out hisrot);
+        float dist = Vector3.Distance(myloc,hisloc);
+        if (dist <= 5 && !isplaying)
+        {
+            isplaying = true;
+            AudioSource.PlayClipAtPoint(audioclip, myloc);
+        }
     }
+
+    
 }
